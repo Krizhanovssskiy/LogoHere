0206fd4266466bec4a555723e82165c0d9ebd4d1
Krizhanovssskiy <Krizhanovssskiy@gmail.com>
2018-03-13 00:17:09 +0200

    finish

--- app/js/main.js
+++ app/js/main.js
@@ -3,3 +3,12 @@ $('header .cmn-toggle-switch').click(function(){
     $('nav').toggleClass('active');
 });
 
+var $grid = $('.grid').imagesLoaded( function() {
+    // init Masonry after all images have loaded
+    $grid.masonry({
+        // options...
+        percentPosition: true,
+        columnWidth: '.grid-sizer',
+        itemSelector: '.grid-item'
+    });
+});
